6652061b99458c6367e86d5d1f4decb8a9491d13
Gilles Rodts <gillesrodts@gmail.com>
2023-02-12 17:34:14 +0100

    added minor tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@ const int cubeCoutY = 8;
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(cubeCountx*64, cubeCoutY*64), "FarmTheClock", sf::Style::Close);
-
+    sf::Clock clock;
     while (window.isOpen())
     {
+        float delta_time = clock.restart().asSeconds();
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -19,9 +21,6 @@ int main()
 
         window.clear(sf::Color::Black);
 
-        // draw everything here...
-
-        // end the current frame
         window.display();
     }
 
